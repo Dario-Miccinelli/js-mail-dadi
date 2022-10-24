99a27155edc86747e04884cd66676dde9781e939
Dario-Miccinelli <poduzzo@hotmail.com>
2022-10-24 17:42:13 +0200

    finita parte mail

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,35 @@
+// MAIL ARRAY PART
 
 
 
-
-  const mailList = ["ratatouille@gmail.com", "boolando@outlook.com", "mucca@yahoo.it"];
+  const mailList = ["nasa@gmail.com", "boolando@outlook.com", "mucca@yahoo.it"];
   const userMail = document.getElementById("inputMail")
-  const output = document.createElement('output');
+  const output = document.querySelector('h2');
+  const verfyButton = document.getElementById('button');
 
 
+  verfyButton.addEventListener('click', function(){
+    console.log(userMail.value);
+    //scorro nell'array per verificare che sia presente l'email inserita
+    let NasaGuest = false;
+    output.innerHTML = "";
+    for (let i = 0; i < mailList.length; i++) {
+      if(userMail.value === mailList[i]){
+        NasaGuest = true;
+      }
+    }
+    //se presente messaggio positivo
+    //se non presente messaggio negativo
+    if(NasaGuest){
+      output.innerHTML = `Welcome! Ur data are stored in NASA database`;
+      userMail.value = "";
+    }else{
+      output.innerHTML = `Sorry, you are not in NASA database.`;
+      userMail.value = "";
+    }
+  });
 
+// MAIL ARRAY ENDS
+
+// DICE GAME
 

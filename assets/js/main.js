755c4f6436d4dd6a2850f7ea51b946d06b261113
Dario-Miccinelli <poduzzo@hotmail.com>
2022-10-24 17:20:18 +0200

    setup inizial

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+
+
+
+  const mailList = ["ratatouille@gmail.com", "boolando@outlook.com", "mucca@yahoo.it"];
+  const userMail = document.getElementById("inputMail")
+  const output = document.createElement('output');
+
+
+
+
